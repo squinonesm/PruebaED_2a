c75cf67045329b23978439062128091f212e6c69
unknown <squinonesm98@gmail.com>
2024-04-09 19:36:54 +0200

    Solucionado un error por el cual el gitignore estaba excluyendo la carpeta target, donde se aloja el javadoc

---
+++ target/site/apidocs/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"ies.comercio.instituto","l":"Curso"},{"p":"ies.comercio.instituto","l":"Instituto"},{"p":"ies.comercio.instituto","l":"Nif"},{"p":"ies.comercio.instituto","l":"Persona"}];updateSearchResults();
